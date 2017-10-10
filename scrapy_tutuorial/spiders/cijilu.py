51971a684207c2d5c421847f441841f9d3485527
qiaozhanlei <qiaozhanlei@baofeng.com>
2017-10-10 18:44:25 +0800

    抽取了首页的视频地址了

--- scrapy_tutuorial/spiders/cijilu.py
+++ scrapy_tutuorial/spiders/cijilu.py
@@ -1,9 +1,25 @@
 import scrapy
 
 
-class CijiluSpider(scrapy.spider):
+class CijiluSpider(scrapy.Spider):
 	name = 'cijilu'
 	start_urls = [
 		'http://www.yemalu.in/'
 	]
-	
+	def parse(self, response):
+		host = self.start_urls[0]
+		urls = []
+		for link in response.css('div.well-sm a'):	
+			url =host + link.css('a::attr(href)').extract_first()
+			urls.append(url)
+		for video_url in urls:
+			video_page = response.urljoin(video_url)
+			yield scrapy.Request(video_url, callback=self.parse_video)
+
+	def parse_video(self, response):
+		mp4_download_url = response.css('ul.dropdown-menu')[-1]
+		video_urls = mp4_download_url.css('a::attr(href)').extract()
+		print '---------------------'
+		print video_urls
+		print '---------------------'
+		
